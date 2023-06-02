77e9a6867f825689c99c93a66981694a8c5005b5
Dania Gord1y Gordienko <95370285+Gord1y@users.noreply.github.com>
2023-06-02 15:06:14 +0300

    feature add section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { FC } from 'react'
 
 import { Line } from '@/components/ui/Line'
@@ -30,6 +31,32 @@ const HomePage: FC = () => {
 				description='Not only do we offer a wide range of recipes, but Manus is also committed to providing you with detailed step-by-step instructions, accompanied by beautiful visuals, ensuring that you can recreate these culinary masterpieces with ease and confidence. We understand that cooking is an immersive experience, and our goal is to guide you through every stage, from selecting the finest ingredients to mastering intricate cooking techniques.'
 				secondaryText="At Manus, we embrace culinary innovation and strive to keep pace with evolving food trends. Our recipe collection is constantly updated with fresh and exciting creations, ensuring that you are always at the forefront of culinary excellence. Whether you're looking for a quick weeknight meal, planning a dinner party, or seeking inspiration for a special occasion, Manus has the perfect recipe to suit your needs."
 			/>
+			<Line />
+			<section className='flex flex-col items-center w-full lg:flex-row text-white'>
+				<div className=' h-fit w-full flex flex-col gap-2 bg-red-400 items-center justify-center py-5 px-10'>
+					<div className=' text-4xl font-bold'>Feel The Taste</div>
+					<div className=' max-w-xs lg:max-w-3xl text-lg font-medium'>
+						Taste is primarily sensed by the taste buds on the tongue, which can
+						detect five basic tastes: sweet, sour, salty, bitter, and umami
+						(savory).
+						<br />
+						<br /> Sweet tastes are often associated with sugary or fruity
+						flavors, while sour tastes are tart and acidic. Salty tastes come
+						from the presence of salt or other minerals, while bitter tastes can
+						be found in foods like coffee, dark chocolate, and leafy greens.
+						Umami tastes are savory and often found in meat, cheese, and other
+						protein-rich foods.
+					</div>
+					<button className=' text-lg py-3 px-5 border-r-2 bg-white lg:bg-transparent text-red-500 lg:text-white border-white hover:bg-white hover:text-red-500 ease-in-out font-semibold'>
+						View Recipes
+					</button>
+				</div>
+				<Image
+					src={img}
+					alt='Picture of the author'
+					className='w-full lg:w-2/5'
+				/>
+			</section>
 		</>
 	)
 }

5a18323d8daa782c7bbfab8670f4c620f1786965
Dania "Gord1y" Gordienko <95370285+Gord1y@users.noreply.github.com>
2023-06-08 01:33:26 +0300

    feature err 404 if no recipe by slug

--- app/recipe/[slug]/page.tsx
+++ app/recipe/[slug]/page.tsx
@@ -1,24 +1,25 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 import { instance } from '@/src/api/api.interceptor'
 
 function NavigationEvents() {
 	const pathname = usePathname()
+	const router = useRouter()
 	const [recipe, setRecipe] = useState({} as any)
 	useEffect(() => {
 		const finc = async () => {
-			const res = await instance.get(pathname)
-			if (res.data) {
-				setRecipe(res.data)
-			}
+			const res = (await instance.get(pathname).catch(err => {
+				router.push('/404')
+			})) as any
+			setRecipe(res.data)
 		}
 		finc()
-	}, [pathname])
+	}, [pathname, router])
 
-	return <>{recipe.name}</>
+	return <>{recipe?.name}</>
 }
 
 export default NavigationEvents

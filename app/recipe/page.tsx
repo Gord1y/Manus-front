e19a9aa42b3456673151f666c4172460f124daa9
Dania "Gord1y" Gordienko <95370285+Gord1y@users.noreply.github.com>
2023-06-08 20:53:47 +0300

    main page

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,7 +1,46 @@
-import { FC } from 'react'
+'use client'
 
-const page: FC = ({}) => {
-	return <div>page</div>
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+import { instance } from '@/src/api/api.interceptor'
+
+interface ICategory {
+	id: number
+	name: string
+	slug: string
+}
+function RecipesPage() {
+	const [categories, setCategories] = useState<ICategory[]>([
+		{ name: 'category', slug: '', id: 0 }
+	])
+	useEffect(() => {
+		const fetchData = async () => {
+			const categories = await instance.get('category/all')
+			if (categories.data) {
+				setCategories(categories.data)
+			}
+		}
+		fetchData()
+	}, [])
+	return (
+		<div className='w-full overflow-hidden'>
+			<div className='flex w-full justify-center text-2xl'>Categories</div>
+			<div className='flex gap-2 items-center w-11/12 overflow-x-auto flex-wrap justify-center mx-auto mt-3'>
+				{categories.map(el => {
+					return (
+						<Link
+							href={'category/' + el.slug}
+							key={el.id}
+							className='w-24 min-w-fit py-2 text-lg px-2 border border-black rounded-md hover:bg-gray-200 text-center'
+						>
+							{el.name}
+						</Link>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
 
-export default page
+export default RecipesPage

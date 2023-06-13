c7f06e6c977aa04f499d18a2b732ce68e9afd132
Dania Gord1y Gordienko <95370285+Gord1y@users.noreply.github.com>
2023-06-13 14:59:50 +0300

    added hooks

---
+++ src/hooks/useAuthRedirect.ts
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+import { useAuth } from './useAuth'
+
+export const useAuthRedirect = () => {
+	const { user } = useAuth()
+	const router = useRouter()
+
+	useEffect(() => {
+		if (user) {
+			router.replace('/dashboard')
+		}
+	}, [router, user])
+}

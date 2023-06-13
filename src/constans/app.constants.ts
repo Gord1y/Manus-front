7c0d09155a8b70cab582f558e8d4c8c761227ade
Dania Gord1y Gordienko <95370285+Gord1y@users.noreply.github.com>
2023-06-13 15:00:00 +0300

    constans add

---
+++ src/constans/app.constants.ts
@@ -0,0 +1,6 @@
+export const SITE_NAME = 'Manus by Gord1y'
+
+export const SITE_DESCRIPTION =
+	'Free site for everyone who want to get perfect recipe for his dinner'
+
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }

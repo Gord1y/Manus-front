9f659e009947d7089edc43d89f475a8db90d5995
Dania Gord1y Gordienko <95370285+Gord1y@users.noreply.github.com>
2023-06-14 15:53:51 +0300

    feature dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,40 @@
+'use client'
+
+import Link from 'next/link'
+
+import { useAuth } from '@/src/hooks/useAuth'
+
 export default function Dashboard() {
-	return <>dashboard</>
+	const { user } = useAuth()
+	return (
+		<>
+			<div className='flex items-center flex-col'>
+				<h1 className='text-3xl'>User</h1>
+				<div className='flex flex-col gap-1'>
+					<p>Email: {user?.email}</p>
+					<p>Name: {user?.name}</p>
+					<p>Surname:{user?.surname}</p>
+				</div>
+				<div className='rounded border border-black py-1 px-3 mt-2'>
+					Change profile
+				</div>
+				{user?.isAdmin ? (
+					<>
+						<Link
+							href='/admin'
+							className='rounded border border-black py-1 px-3 mt-2'
+						>
+							Admin Panel
+						</Link>
+					</>
+				) : (
+					<>
+						<button className='rounded border border-black py-1 px-3 mt-2'>
+							Suggest a new recipe
+						</button>
+					</>
+				)}
+			</div>
+		</>
+	)
 }
